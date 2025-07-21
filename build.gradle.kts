9e9fb49471265850228cf157b6298884d350ef56
osamax2009 <osamafalah85@yahoo.com>
2025-07-21 21:16:48 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.5.21" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
