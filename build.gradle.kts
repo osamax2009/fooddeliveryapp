1c9edf792a7cbbd301cd496aace0af4b24219e85
osamax2009 <osamafalah85@yahoo.com>
2025-09-03 18:27:00 +0300

    mdfdf

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.5.21" apply false
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false  // Add KSP
+    id("com.google.dagger.hilt.android") version "2.48" apply false  // Updated version
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22" apply false  // Updated version
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
